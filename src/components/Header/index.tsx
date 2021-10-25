834cdb20ec59e8f8a8c045775fde2f8776c4d450
Nicolas Castanha <nicolascastanha2002@gmail.com>
2021-10-25 15:15:11 -0300

    Alteração no Header 1.1

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,11 @@ const Header: React.FC = () => {
       <Container>
         <Navbar.Brand href="/">Home</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/tarefas">Tarefas</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
