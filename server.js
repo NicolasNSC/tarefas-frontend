9ed0abae8faa4de16995194c614f422909192ee9
Nicolas Castanha <nicolascastanha2002@gmail.com>
2021-10-25 15:03:25 -0300

    Deploy no Heroku 1.2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+ 
+app.use(express.static('build'))
+app.listen(PORT, () => console.log('Servidor Front em execução!'))
 
-app.use(express.static.apply("build"));
-app.listen(PORT, () => console.log("Servidor Front em execução!"));
